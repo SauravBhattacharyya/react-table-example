b6d58d5ffc0cd2d6d3cc4e920fc1097a1415f440
Saurav Bhattacharyya <sauravbhattacharyya2@gmail.com>
2021-11-08 09:41:45 +0530

    table column

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,10 @@
 import React, { useMemo } from "react";
-import { tableData } from "./data";
+import { tableColumn } from "./utils/tableColumnUtil";
+import { tableData } from "./utils/tableDataUtil";
 import TableContainer from "./tableContainer";
 
 const App = () => {
-  const columns = useMemo(
-    () => [
-      { Header: "First Name", accessor: "first_name" },
-      { Header: "Last Name", accessor: "last_name" },
-      { Header: "Email", accessor: "email" },
-      { Header: "Phone", accessor: "phone" },
-      { Header: "Date of Birth", accessor: "date_of_birth" },
-      { Header: "Sex", accessor: "sex" },
-    ],
-    []
-  );
-
+  const columns = useMemo(() => tableColumn, []);
   const data = useMemo(() => tableData, []);
 
   return <TableContainer data={data} columns={columns} />;
---
+++ src/utils/tableColumnUtil.jsx
@@ -0,0 +1,8 @@
+export const tableColumn = [
+  { Header: "First Name", accessor: "first_name" },
+  { Header: "Last Name", accessor: "last_name" },
+  { Header: "Email", accessor: "email" },
+  { Header: "Phone", accessor: "phone" },
+  { Header: "Date of Birth", accessor: "date_of_birth" },
+  { Header: "Sex", accessor: "sex" },
+];
